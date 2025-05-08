b3d3bcfa93eb5b022b2bbe38dca8ec4f9e1bc653
valeriam07 <valeriamorales40@gmail.com>
2025-05-08 17:08:24 -0600

    Calculo y medidas para aumentar FPS

--- tof_detection/realTimeProcessing.py
+++ tof_detection/realTimeProcessing.py
@@ -13,6 +13,7 @@ import ArducamDepthCamera as ac
 MAX_DISTANCE = 4000  # Distancia máxima de detección en mm
 GRID_SIZE = 9        # Grid 9x9
 
+# Forma de visualizar las zonas detectadas como obstaculo dentro del grid de la imagen
 def draw_grid_overlay(image, prediction_grid):
     h, w, _ = image.shape
     cell_h = h // GRID_SIZE
@@ -29,7 +30,28 @@ def draw_grid_overlay(image, prediction_grid):
             if prediction_grid[i, j] == 1:
                 color = (0, 0, 255)  # Rojo si hay obstáculo
 
-            cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+            cv2.rectangle(image, (x1, y1), (x2, y2), color, 1)
+
+    return image
+
+# Forma de visualizar los obstaculos detectados: marca el area donde hay obstaculo
+def draw_obstacle_overlay(image, prediction_grid):
+    h, w, _ = image.shape
+    cell_h = h // GRID_SIZE
+    cell_w = w // GRID_SIZE
+    obstacle_rectangles = []
+
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            if prediction_grid[i, j] == 1:  # Si hay obstaculo
+                y1 = i * cell_h
+                y2 = (i + 1) * cell_h
+                x1 = j * cell_w
+                x2 = (j + 1) * cell_w
+                obstacle_rectangles.append((x1, y1, x2, y2))
+
+    for (x1, y1, x2, y2) in obstacle_rectangles:
+        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 0), 1)  # Rectangulo celeste
 
     return image
 
@@ -55,16 +77,24 @@ def main():
     B = np.load("../model/saved_models/v4/biases.npy")
 
     print("Presiona 'q' para salir.")
-
+    
+    prev_time = time.time()
+    
+    # Establecer el size de la ventana
+    cv2.namedWindow("ToF Grid Detection", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("ToF Grid Detection", 1024, 768)  
+    
     while True:
         frame = cam.requestFrame(2000)
+        
         if frame and isinstance(frame, ac.DepthData):
             depth_buf = frame.depth_data
             depth_image = (depth_buf * (255.0 / r)).astype(np.uint8)
             colorized = cv2.applyColorMap(depth_image, cv2.COLORMAP_RAINBOW)
+            colorized_resized = cv2.resize(colorized, (120, 90))  #(width,height), original shape = (height=240, width=180)
 
             # Predecir grid 9x9
-            conv = model.convolution_alt(colorized, kernel_size=7, stride=3)
+            conv = model.convolution_alt(colorized_resized, kernel_size=3, stride=5)
             relu = model.activation_relu(conv)
             pooled = model.pooling(relu, pool_size=2, stride=2)
 
@@ -88,7 +118,16 @@ def main():
                     prediction_grid[i, j] = np.argmax(probs)
 
             # Dibujar celdas
-            result = draw_grid_overlay(colorized.copy(), prediction_grid)
+            result = draw_obstacle_overlay(colorized.copy(), prediction_grid)
+            
+            # Medir FPS
+            curr_time = time.time()
+            fps = 1 / (curr_time - prev_time)
+            prev_time = curr_time
+
+            # Mostrar FPS en la imagen
+            cv2.putText(result, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
 
             # Mostrar en ventana
             cv2.imshow("ToF Grid Detection", result)
